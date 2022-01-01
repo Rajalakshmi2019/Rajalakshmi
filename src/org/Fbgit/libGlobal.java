49b61b7dd85e4fa4604b847f9155d5de83fe636f
Rajalakshmi2019 <chkaraji.101@gmail.com>
2022-01-01 11:20:17 +0530

    userName functionality

--- src/org/Fbgit/libGlobal.java
+++ src/org/Fbgit/libGlobal.java
@@ -1,6 +1,10 @@
 package org.Fbgit;
 
 public class libGlobal {
-	
-
-}
+	public static void userName() {
+		System.out.println("userName");
+	}
+	public static void main(String[] args) {
+		userName();
+	}
+	}
