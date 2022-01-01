ba707fa9d68ab72e8e50ba6560ef78be039b66d3
Rajalakshmi2019 <chkaraji.101@gmail.com>
2022-01-01 11:42:41 +0530

    password functionality

--- src/org/Fbgit/libGlobal.java
+++ src/org/Fbgit/libGlobal.java
@@ -1,6 +1,11 @@
 package org.Fbgit;
 
 public class libGlobal {
+	public static void password() {
+		System.out.println("password");
+	}
+	public static void main(String[] args) {
+		password();
+	}
+	}
 	
-
-}
