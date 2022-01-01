8b64a5c0e167d6a8d99419fce7611c91ca1c31a8
Rajalakshmi2019 <chkaraji.101@gmail.com>
2022-01-01 12:26:07 +0530

    pass fuctionality

--- src/org/Fbgit/libGlobal.java
+++ src/org/Fbgit/libGlobal.java
@@ -1,10 +1,10 @@
 package org.Fbgit;
 
 public class libGlobal {
-	public static void userName() {
-		System.out.println("userName");
+	public static void pass() {
+		System.out.println("password");
 	}
 	public static void main(String[] args) {
-		userName();
+		pass();
 	}
 	}
